42603d202ee1bd59bd0d7d848219957219afcae3
Ruchika Mahajan <ruchikamahajan66@gmail.com>
2025-08-26 12:57:31 -0700

    added d matrix to the ase db

--- src/AutoCatLab/executor/icohp_executor.py
+++ src/AutoCatLab/executor/icohp_executor.py
@@ -47,7 +47,7 @@ class ICOHPExecutor(CalculationExecutor):
 
         I_matrix, d_matrix = get_icohp_matrix(atoms, str(folder) + '/ICOHPLIST.lobster')  # take all orbitals for now
         I_sum = np.sum(I_matrix, axis=1)
-        d_sum = np.sum(d_matrix, axis=1)
+        d_avg = np.mean(d_matrix, axis=1)
         I_matrix_s_d = get_icohp_matrix(atoms, str(folder) + '/ICOHPLIST.lobster',
                                         orbitals=orbital)  # take all orbitals for now
         I_sum_s_d = np.sum(I_matrix_s_d, axis=1)
@@ -81,7 +81,7 @@ class ICOHPExecutor(CalculationExecutor):
                         execution.calculation_name: {
                             'icohp_matrix': I_matrix,
                             'icohp_sum': I_sum,
-                            'distance_sum': d_sum,
+                            'distance_avg': d_avg,
                             'icohp_matrix_s_d': I_matrix_s_d,
                             'icohp_sum_s_d': I_sum_s_d,
                             'icohp_sum_s_d_o_2s_2p': I_sum_s_d_o_2s_2p,
